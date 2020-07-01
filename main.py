819f237fb2cc17abf7598b494fd2cc559c7f415f
Neha Joshi <40416505+joshinehajoshi@users.noreply.github.com>
2020-07-01 12:39:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import turtle
+wn = turtle.Screen()
+wn.title("Pong Game by NehaJoshi.")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+# Paddle A
+
+
+
+# Paddle B
+
+
+
+
+# Ball
+
+
+
+# Main game loop
+while True:
+    wn.update()
